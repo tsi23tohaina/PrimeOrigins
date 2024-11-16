fdc938927d89d7556c49909247b8d08cbc4d11d0
THE-AUDIO <nandraina.dev22@gmail.com>
2024-11-16 20:42:08 +0300

    finished login

--- PrimeOriginClient/src/app/user/component/auth/auth.component.ts
+++ PrimeOriginClient/src/app/user/component/auth/auth.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-auth',
   standalone: false,
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.scss'
+  styleUrl: './auth.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+
 })
 export class AuthComponent {
-
+  constructor(
+    private formBuilder: FormBuilder,
+  ){}
+ 
+  authGroup!: FormGroup;
+  usernameCtrl!: FormControl;
+  passwordCtrl!: FormControl;
+  loading:Boolean = false;
+  initAuthForm(){
+    this.usernameCtrl = this.formBuilder.control("");
+    this.usernameCtrl.addValidators([Validators.required]);
+    this.passwordCtrl = this.formBuilder.control("");
+    this.passwordCtrl.addValidators([Validators.required, Validators.minLength(8)]);
+    this.authGroup = this.formBuilder.group({
+      username: this.usernameCtrl,
+      password: this.passwordCtrl,
+    })
+  }
+  ngOnInit(): void {
+    this.initAuthForm();
+  }
+  onSubmit(){
+    console.log(this.authGroup.value);
+  }
 }
--- PrimeOriginClient/src/app/user/user.module.ts
+++ PrimeOriginClient/src/app/user/user.module.ts
@@ -1,21 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { UserRoutingModule } from './user-routing.module';
 import { SharedModule } from '../shared/shared.module';
-import { AuthComponent } from './component/auth/auth.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AuthComponent } from './component/auth/auth.component';
 
 
 @NgModule({
   declarations:[
-    AuthComponent
+    AuthComponent,
   ],
   imports: [
     CommonModule,
     UserRoutingModule,
     SharedModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ]
 })
 export class UserModule { }
