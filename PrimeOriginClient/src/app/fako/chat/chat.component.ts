d544a06702c0878f3dc7e2f35ea7fed38ac07c45
THE-AUDIO <nandraina.dev22@gmail.com>
2024-11-17 03:26:48 +0300

    add chat module and implement

--- PrimeOriginClient/src/app/fako/chat/chat.component.ts
+++ PrimeOriginClient/src/app/fako/chat/chat.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FakoService } from '../fako.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-chat',
@@ -8,37 +8,49 @@ import { FakoService } from '../fako.service';
 })
 export class ChatComponent {
   loading = false;
-  data = new FormData();
   fileName: string | null = null;
+  formData = new FormData();
+  responseMessage: string = ''; 
+  questionState = true;
 
-  constructor(private fakoService: FakoService) {}
-   formData = new FormData();
+  constructor(private http: HttpClient) {}
 
   onFileSelected(event: Event): void {
     const input = event.target as HTMLInputElement;
-  
+    
     if (input.files && input.files.length > 0) {
       const file = input.files[0];
       this.fileName = file.name;
-  
+      // Ajoutez les autres champs
       this.formData.append('file', file);
+      this.formData.append('sms', 'Donner une deuxime vie à cette image en proposant 3 ideé en forme ');
+      // this.formData.append('iteration', '1'); // ou autre valeur
     } else {
       this.fileName = null;
     }
   }
+  
 
-  analyse(){
-    this.loading = true;
-    this.fakoService.analyseImage(this.formData).subscribe(
-      (result: any) => {
-        this.loading = false;
-      },
-      (error: any) => {
-       console.log(error);
-       this.loading = false;
+  analyse(): void {
+    console.log(this.formData);
+    
+    if (this.formData.has('file')) {  // Vérifier que le fichier est bien ajouté à formData
+      this.loading = true;
+      this.http.post('http://10.166.2.156:8000/upload', this.formData).subscribe(
+        (response:any) => {
 
-      }
-    )
+          console.log('Upload réussi', response);
+          this.loading = false;
+          this.responseMessage = response.response;
+          this.questionState=false;
+        },
+        error => {
+          console.error('Erreur lors de l\'upload', error);
+          this.loading = false;
+        }
+      );
+    } else {
+      console.error('Aucun fichier sélectionné.');
+    }
   }
-
-  }
+}
