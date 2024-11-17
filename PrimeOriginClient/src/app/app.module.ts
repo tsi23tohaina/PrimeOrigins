485a3838649001ccec4fe7604548896fabebfd71
THE-AUDIO <nandraina.dev22@gmail.com>
2024-11-17 09:49:51 +0300

    add component presentation

--- PrimeOriginClient/src/app/app.module.ts
+++ PrimeOriginClient/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
+import { PresentationComponent } from './presentation/presentation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PresentationComponent
   ],
   imports: [
     BrowserModule,
